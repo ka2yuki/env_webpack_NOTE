0b05e85c3d76929d80c6a8e6f5e874d4719ee84a
ka2yuki <mori1103k@gmail.com>
2024-08-10 19:52:39 +0900

    ESLinter for webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,30 @@
 // Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const WorkboxWebpackPlugin = require("workbox-webpack-plugin");
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const WorkboxWebpackPlugin = require('workbox-webpack-plugin')
 
-const isProduction = process.env.NODE_ENV == "production";
+const isProduction = process.env.NODE_ENV === 'production'
 
 const stylesHandler = isProduction
   ? MiniCssExtractPlugin.loader
-  : "style-loader";
+  : 'style-loader'
 
 const config = {
-  entry: "./src/index.ts",
+  entry: './src/index.ts',
   output: {
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, 'dist')
   },
   devServer: {
     open: true,
-    host: "localhost",
+    host: 'localhost',
+    historyApiFallback: true
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: "index.html",
-    }),
+      template: 'index.html'
+    })
 
     // Add your plugins here
     // Learn more about plugins from https://webpack.js.org/configuration/plugins/
@@ -32,40 +33,40 @@ const config = {
     rules: [
       {
         test: /\.(ts|tsx)$/i,
-        loader: "ts-loader",
-        exclude: ["/node_modules/"],
+        loader: 'ts-loader',
+        exclude: ['/node_modules/']
       },
       {
         test: /\.css$/i,
-        use: [stylesHandler, "css-loader"],
+        use: [stylesHandler, 'css-loader']
       },
       {
         test: /\.styl$/i,
-        use: [stylesHandler, "css-loader", "stylus-loader"],
+        use: [stylesHandler, 'css-loader', 'stylus-loader']
       },
       {
         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: "asset",
-      },
+        type: 'asset'
+      }
 
       // Add your rules for custom modules here
       // Learn more about loaders from https://webpack.js.org/loaders/
-    ],
+    ]
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", "..."],
-  },
-};
+    extensions: ['.tsx', '.ts', '.jsx', '.js', '...']
+  }
+}
 
 module.exports = () => {
   if (isProduction) {
-    config.mode = "production";
+    config.mode = 'production'
 
-    config.plugins.push(new MiniCssExtractPlugin());
+    config.plugins.push(new MiniCssExtractPlugin())
 
-    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW());
+    config.plugins.push(new WorkboxWebpackPlugin.GenerateSW())
   } else {
-    config.mode = "development";
+    config.mode = 'development'
   }
-  return config;
-};
+  return config
+}
